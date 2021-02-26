b19e2e03a22daab7b9d57ea9b4315c9df3563fbc
Nobo <nabacse@linkstaff.co.jp>
2021-02-26 16:47:10 +0900

    models have been almost completed.

--- eCommerceApi/models.py
+++ eCommerceApi/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    quantity = models.IntegerField(default=0, null=False)
+    remainQuantity = models.IntegerField(default=0, null=False)
     description = models.TextField(max_length=500)
     hoverImage = models.ImageField(upload_to='products')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -13,6 +13,29 @@ class Products(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Orders(models.Model):
+class Order(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
+    companyName = models.CharField(max_length=50)
+    countryName = models.CharField(max_length=25)
+    shippingAddress = models.CharField(max_length=255)
+    town = models.CharField(max_length=50)
+    zipCode = models.CharField(max_length=20)
+    phoneNo = models.CharField(max_length=20)
+    orderComment = models.CharField(max_length=255)
+    otherShippingAddress = models.CharField(max_length=255)
+    paymentMethodType = models.CharField(max_length=20)
+    subTotal = models.IntegerField(default=0, null=False)
+    delivery = models.IntegerField(default=0, null=False)
+    total = models.IntegerField(default=0, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through='OrderDetail')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
